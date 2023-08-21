de74ec4ac5e8d910d0d385700815820328c2c8b9
jennifer <jennifertorrespalomino@gmail.com>
2023-08-20 23:50:02 -0500

    Nueva actualización

--- app/src/main/java/com/jennifer/ef_torrespalomino/MainActivity.java
+++ app/src/main/java/com/jennifer/ef_torrespalomino/MainActivity.java
@@ -1,14 +1,49 @@
 package com.jennifer.ef_torrespalomino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.jennifer.ef_torrespalomino.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
+    public static String EMAIL = "EMAIL";
+    private String email;
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.store_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if (item.getItemId() == R.id.favorite) {
+            Snackbar.make(binding.getRoot(), "Favorite", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }else if (item.getItemId() == R.id.logout) {
+            sharedPreferences.edit().clear().apply();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return false;
     }
 }
